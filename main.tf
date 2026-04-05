480acfdafccded81c51f81b80c802dfdc7964ddd
igbalaye <igbalayek@gmail.com>
2026-04-05 23:32:54 +0100

    updates4598

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "aws_instance" "web" {
 
               # Login to ECR (region specific)
               REGION="eu-west-2"
-              ACCOUNT_ID=$(curl http://169.254.169.254/latest/meta-data/identity-credentials/ec2/info | grep AccountId | awk -F'"' '{print $4}')
+              ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
               aws ecr get-login-password --region $REGION \
                 | docker login --username AWS \
